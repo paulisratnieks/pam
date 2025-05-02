0c6bbdfd59452619df9581fadcd9b4324500823d
Paulis Ratnieks <paulis.ratnieks7@gmail.com>
2025-05-02 06:42:24 +0300

    refactor main.go to use cli flags, add write to file option, debug, input options

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -9,34 +10,66 @@ import (
 )
 
 func main() {
-	b, err := os.ReadFile("pam.txt")
+	log.SetFlags(0)
+	input := flag.String("i", "", "input file")
+	output := flag.String("o", "", "output file")
+	debug := flag.Bool("d", false, "debug mode")
+	flag.Parse()
+	source := flag.Arg(0)
+
+	if source == "" {
+		log.Fatal("usage: pam [-i input file] [-o output file] [-d debug] file")
+	}
+
+	b, err := os.ReadFile(source)
 	if err != nil {
 		log.Fatalf("error opening source file: %v", err)
 	}
-
-	lexer := NewLexer(string(b))
-	tokens, _ := lexer.Scan()
-	parser := NewParser(tokens)
-	ast := parser.Parse()
-	ast.Print()
-
-	input := make([]int, 0)
-	b, err = os.ReadFile("input.txt")
-	if err == nil {
-		for _, s := range strings.Split(string(b), ",") {
-			s = strings.TrimSpace(s)
-			i, err := strconv.Atoi(s)
-			if err != nil {
-				log.Fatalf("input contains unparsable int chars: %v", s)
+	var inp []int
+	if *input != "" {
+		b, err := os.ReadFile(*input)
+		if err == nil {
+			for _, s := range strings.Split(string(b), ",") {
+				s = strings.TrimSpace(s)
+				i, err := strconv.Atoi(s)
+				if err != nil {
+					log.Fatalf("input contains unparsable int chars: %v", s)
+				}
+				inp = append(inp, i)
 			}
-			input = append(input, i)
 		}
 	}
 
-	interpreter := NewInterpreter(input)
-	interpreter.Interpret(ast)
+	lexer := NewLexer(string(b))
+	tokens, errs := lexer.Scan()
+	if errs != nil {
+		log.Fatalf("errors when tokenizing input: %v", errs)
+	}
 
-	for _, i := range interpreter.output {
-		fmt.Println(i)
+	parser := NewParser(tokens)
+	ast := parser.Parse()
+	if *debug {
+		log.Println(ast.Print())
+	}
+
+	interpreter := NewInterpreter(inp)
+	interpreter.Interpret(ast)
+	if *output != "" {
+		f, err := os.Create(*output)
+		if err != nil {
+			log.Fatalf("error creating output file: %v", err)
+		}
+		defer f.Close()
+
+		for _, i := range interpreter.output {
+			_, err = fmt.Fprintln(f, strconv.Itoa(i))
+			if err != nil {
+				log.Fatalf("error writing to output file: %v", err)
+			}
+		}
+	} else {
+		for _, i := range interpreter.output {
+			log.Println(i)
+		}
 	}
 }
